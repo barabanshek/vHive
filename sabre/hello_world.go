834d94897adf7f1d36d57ec2e353aeedc8ba741e
Nikita Lazarev <niklz@csail.mit.edu>
2024-04-16 08:52:55 -0700

    hello_world improveds

--- sabre/hello_world.go
+++ sabre/hello_world.go
@@ -5,10 +5,12 @@ import (
 	"flag"
 	"math/rand"
 	"time"
+	"fmt"
 
 	"github.com/containerd/containerd/namespaces"
 	log "github.com/sirupsen/logrus"
 	ctriface "github.com/vhive-serverless/vhive/ctriface"
+	"github.com/vhive-serverless/vhive/snapshotting"
 )
 
 // Configs.
@@ -30,12 +32,78 @@ func generateRandomString(length int) string {
 	return string(b)
 }
 
+// Functions.
+func startVM(orch *ctriface.Orchestrator, ctx context.Context, image_name string, mem_size int) (error, string) {
+	vmID := generateRandomString(10)
+	rsp, metrics, err := orch.StartVM(ctx, vmID, image_name, mem_size)
+	if err != nil {
+		return fmt.Errorf("failed to StartVM: ", err), ""
+	}
+	log.Info("VM started, IP= %v", rsp.GuestIP)
+	metrics.PrintAll()
+
+	return nil, vmID
+}
+
+// Examples.
+func stop(orch *ctriface.Orchestrator, ctx context.Context, vmID string) error {
+	if err := orch.StopSingleVM(ctx, vmID); err != nil {
+		return fmt.Errorf("failed to stopVM.", err)
+	}
+	log.Info("VM stopped")
+
+	return nil
+}
+
+func snapshot_basic(orch *ctriface.Orchestrator, ctx context.Context, vmID string, image_name string, mem_size int) error {
+	if err := orch.PauseVM(ctx, vmID); err != nil {
+		return fmt.Errorf("failed to PauseVM, error was: %v", err)
+	}
+	log.Info("VM paused")
+
+	revision := "myrev-4"
+	snap := snapshotting.NewSnapshot(revision, "/fccd/snapshots", image_name)
+	if err := snap.CreateSnapDir(); err != nil {
+		return fmt.Errorf("failed to CreateSnapDir %v", err)
+	}
+
+	if err := orch.CreateSnapshot(ctx, vmID, snap); err != nil {
+		return fmt.Errorf("failed to CreateSnapshot %v", err)
+	}
+	log.Info("VM snapshotted")
+
+	if _, err := orch.ResumeVM(ctx, vmID); err != nil {
+		return fmt.Errorf("failed to ResumeVM %v", err)
+	}
+	log.Info("VM resumed")
+
+	if err := orch.StopSingleVM(ctx, vmID); err != nil {
+		return fmt.Errorf("failed to stopVM.", err)
+	}
+	log.Info("VM stopped")
+
+	if _, metrics, err := orch.LoadSnapshot(ctx, vmID, snap, mem_size); err != nil {
+		return fmt.Errorf("failed to LoadSnapshot %v", err)
+	} else {
+		log.Info("VM loaded from the snapshot:")
+		metrics.PrintAll()
+	}
+
+	if err := orch.StopSingleVM(ctx, vmID); err != nil {
+		return fmt.Errorf("failed to stopVM.", err)
+	}
+	log.Info("VM stopped")
+
+	return nil
+}
+
 func main() {
 	log.Info("Starting e2e experiments.")
 
 	// Parse inputs.
-	testImageNameFlag := flag.String("image", "image", "image")
+	testImageNameFlag := flag.String("image", "image", "image name")
 	testMemorySizeFlag := flag.Int("memsize", 256, "memory size in MB")
+	testExampleNameFlag := flag.String("example", "start-stop", "example to execuute")
 	flag.Parse()
 
 	// Initialize the context.
@@ -56,23 +124,25 @@ func main() {
 		ctriface.WithLazyMode(*isLazyMode),
 	)
 
-	// Start a uVM.
-	vmID := generateRandomString(10)
-	rsp, metrics, err := orch.StartVM(ctx, vmID, *testImageNameFlag, *testMemorySizeFlag)
-	if err != nil {
-		log.Fatalf("failed to StartVM %v", err)
+	//
+	// Start a uVM and run examples.
+	//
+	error_ := fmt.Errorf("")
+	if err, vmID := startVM(orch, ctx, *testImageNameFlag, *testMemorySizeFlag); err == nil {
+		// Run experiments.
+		switch *testExampleNameFlag {
+		case "start-stop": error_ = stop(orch, ctx, vmID)
+		case "start-snapshot-stop-resume-stop": error_ = snapshot_basic(orch, ctx, vmID, *testImageNameFlag, *testMemorySizeFlag)
+		default: {
+			log.Info("Unknown experiment.")
+			error_ = stop(orch, ctx, vmID)
+		}
+		}
+	};
+
+	if error_ == nil {
+		log.Info("Examples finished, bye!")
+	} else {
+		log.Fatal("Examples crashed.")
 	}
-	log.Info("VM started, IP= %v", rsp.GuestIP)
-	metrics.PrintAll()
-
-	time.Sleep(5 * time.Second)
-
-	// Stop the uVM.
-	err = orch.StopSingleVM(ctx, vmID)
-	if err != nil {
-		log.Fatalf("failed to StopVM %v", err)
-	}
-	log.Info("VM stopped")
-
-	log.Info("The e2e experimens are finished, bye!")
 }
